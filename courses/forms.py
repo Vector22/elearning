30db0f169ad52fc08dfbcc6e3b558dad4ba36cbb
Ulrich <ulrich@ipeos.com>
2021-01-13 18:37:19 +0000

    CRUD on course plus formset to add modules to course done.

---
+++ courses/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+
+from .models import Course, Module
+
+# Since a course is divided into a variable number of modules,
+# it makes sense to use formsets to manage them.
+# The inlineformset_factory  function allows us to build a model formset
+# dynamically for the Module objects related to a Course object.
+ModuleFormSet = inlineformset_factory(Course,
+                                      Module,
+                                      fields=['title', 'description'],
+                                      extra=2,
+                                      can_delete=True)
