fff61f591b4f1073e499a1f42ed4dda5846a04f3
Ulrich <ulrich@ipeos.com>
2021-02-16 17:23:58 +0000

    Add Django debug toolbar, CBV + Mixins to manage Courses and Modules objects.

--- elearning/settings.py
+++ elearning/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django debug toolbar
+    'debug_toolbar',
     'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Django debug toolbar
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'elearning.urls'
@@ -117,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django debug toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
